8d24521fed9b3276ddff8fcc06c466bc83b21987
Jake Prins <jakeprins@outlook.com>
2023-02-06 09:55:55 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [vue(), dts({ skipDiagnostics: true })],
 
   build: {
-    outDir: "./dist",
+    outDir: './dist',
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "vue-vault",
-      fileName: (format) => `vue-vault.${format}.js`,
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-file-picker',
+      fileName: (format) => `vue-file-picker.${format}.js`,
     },
     rollupOptions: {
-      external: ["vue"],
-      output: { globals: { vue: "Vue" } },
+      external: ['vue'],
+      output: { globals: { vue: 'Vue' } },
     },
   },
 });
